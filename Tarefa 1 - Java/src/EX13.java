91b5b4888ad2b50150d3063f19c3dc8db8221507
Tiago Santos <137201904+TiagoBernardoSantos@users.noreply.github.com>
2024-08-22 09:51:17 -0300

    Update EX13.java

--- Tarefa 1 - Java/src/EX13.java
+++ Tarefa 1 - Java/src/EX13.java
@@ -14,7 +14,7 @@ public class EX13 {
             System.out.print("Em ordem crescente: " + val1 +", " + val2);
         }
         else if (val2<val1){
-            System.out.print("O maior valor é: " + val2+ ", "+ val1);
+            System.out.print("Em ordem crescente: " + val2+ ", "+ val1);
         }
         else{
             System.out.print("Números iguais.");
